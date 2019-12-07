f6037450829d180be29aa4261ae71eed3c4dcb93
Daniel Gacek <maveriok@student.agh.edu.pl>
2019-12-07 23:46:52 +0100

    validation set fix

--- vgg_unet/train_new.py
+++ vgg_unet/train_new.py
@@ -6,11 +6,12 @@ model = vgg_unet(n_classes=4, input_height=480, input_width=480)
 model.train(
     train_images="./PRZEKROJE/",
     train_annotations="./PRZEKROJE_ANNOTATIONS/",
-    checkpoints_path="./vgg_unet_4classes",
+    checkpoints_path="./vgg_unet_4cl_newlabels",
     epochs=25,
     batch_size=1,
     val_images="./PRZEKROJE_VAL/",
     val_annotations="./PRZEKROJE_ANNOTATIONS_VAL/",
     val_batch_size=1,
-    validate=True
+    validate=True,
+    do_augment=True
 )
